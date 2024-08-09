b355f6fb5e66c1ba110d6c6007a755a4e7687407
wengqinghui <3113744638@qq.com>
2024-08-09 20:48:46 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/nowcoder/community/controller/LoginController.java
    #	src/main/java/com/nowcoder/community/service/UserService.java
    #	src/main/java/com/nowcoder/community/util/CommunityConstant.java
    #	src/main/resources/templates/site/login.html

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -182,4 +182,13 @@ public class AlphaController {
         System.out.println(session.getAttribute("name"));
         return "get session";
     }
+
+    // ajax示例
+    @RequestMapping(path="/ajax", method=RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name, int age){
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJSONString(0, "操作成功！");
+    }
 }
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -12,6 +14,9 @@ public class DiscussPostService {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return  discussPostMapper.selectDiscussPosts(userId, offset, limit);
@@ -22,5 +27,22 @@ public class DiscussPostService {
     }
 
     //1111111
+    public int addDiscussPost(DiscussPost post){
+        if(post == null){
+            throw new IllegalArgumentException("参数不能为空！");
+        }
 
+        // 转义HTML标记
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    public DiscussPost findDiscussPostById(int id){
+        return discussPostMapper.selectDiscussPostById(id);
+    }
 }
