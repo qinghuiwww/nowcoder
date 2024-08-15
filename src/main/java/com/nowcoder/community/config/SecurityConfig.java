81130d48079b7dbf7f85dd706009eaf7dd24094d
wengqinghui <3113744638@qq.com>
2024-08-16 01:04:56 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/nowcoder/community/controller/LoginController.java
    #	src/main/java/com/nowcoder/community/service/UserService.java
    #	src/main/java/com/nowcoder/community/util/CommunityConstant.java
    #	src/main/resources/templates/site/login.html

--- src/main/java/com/nowcoder/community/config/SecurityConfig.java
+++ src/main/java/com/nowcoder/community/config/SecurityConfig.java
@@ -45,6 +45,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements Comm
                         AUTHORITY_ADMIN,
                         AUTHORITY_MODERATOR
                 )
+                .antMatchers(
+                        "/discuss/top",
+                        "/discuss/wonderful"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_MODERATOR
+                )
+                .antMatchers(
+                        "/discuss/delete",
+                        "/data/**"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_ADMIN
+                )
                 .anyRequest().permitAll()
                 .and().csrf().disable();
 
--- src/main/resources/static/js/discuss.js
+++ src/main/resources/static/js/discuss.js
@@ -1,3 +1,9 @@
+$(function() {
+    $("#topBtn").click(setTop);
+    $("#wonderfulBtn").click(setWonderful);
+    $("#deleteBtn").click(setDelete);
+});
+
 function like(btn, entityType, entityId, entityUserId, postId) {
     $.post(
       CONTEXT_PATH + "/like",
@@ -10,6 +16,53 @@ function like(btn, entityType, entityId, entityUserId, postId) {
           }else{
               alert(data.msg);
           }
+        }
+    );
+}
+
+// 置顶
+function setTop(){
+    $.post(
+        CONTEXT_PATH + "/discuss/top",
+        {"id":$("#postId").val()},
+        function (data){
+            data = $.parseJSON(data);
+            if(data.code == 0){
+                $("#topBtn").attr("disabled","disabled");
+            }else{
+                alert(data.msg);
+            }
+        }
+    );
+}
+
+// 加精
+function setWonderful(){
+    $.post(
+        CONTEXT_PATH + "/discuss/wonderful",
+        {"id":$("#postId").val()},
+        function (data){
+            data = $.parseJSON(data);
+            if(data.code == 0){
+                $("#wonderfulBtn").attr("disabled","disabled");
+            }else{
+                alert(data.msg);
+            }
+        }
+    );
+}
+// 删除
+function setDelete(){
+    $.post(
+        CONTEXT_PATH + "/discuss/delete",
+        {"id":$("#postId").val()},
+        function (data){
+            data = $.parseJSON(data);
+            if(data.code == 0){
+                location.href = CONTEXT_PATH + "/index";
+            }else{
+                alert(data.msg);
+            }
         }
     );
 }
